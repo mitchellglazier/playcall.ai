11ab65501d14c366844ea907b264df317d8d04f0
Mitchell Glazier <glazierm@gmail.com>
2020-08-27 01:31:48 -0600

    game page improvements and stats

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -14,7 +14,10 @@ import { MatTableDataSource } from "@angular/material/table";
   styleUrls: ["./game.component.css"],
 })
 export class GameComponent implements OnInit {
+  gamePlaysArray!: MatTableDataSource<any>;
   displayedColumns1: string[] = ["play", "result", "delete"];
+  @ViewChild("TableTwoPaginator", { static: true })
+  tableTwoPaginator!: MatPaginator;
 
   playsArray!: MatTableDataSource<any>;
   displayedColumns2: string[] = ["play", "gameCount", "gameAvg", "seasonAvg"];
@@ -30,14 +33,14 @@ export class GameComponent implements OnInit {
 
   selectPlays: Array<any> = [];
 
-  gamePlaysArray: GamePlay[] = [];
-
   runPlays: GamePlay[] = [];
   totalRushYards!: number;
   passPlays: GamePlay[] = [];
   totalPassYards!: number;
   totalYards!: number;
   avgYards!: string;
+  runAvg!: number;
+  passAvg!: number;
 
   constructor(
     private route: ActivatedRoute,
@@ -45,30 +48,35 @@ export class GameComponent implements OnInit {
     private playsService: PlaysService
   ) {
     this.gameId = this.route.snapshot.paramMap.get("id");
+    this.gamePlaysArray = new MatTableDataSource();
     this.playsArray = new MatTableDataSource();
   }
 
   ngOnInit(): void {
     this.gamesService.getGame(this.gameId).subscribe((game) => {
       this.game = game.payload.data();
-      this.gamePlaysArray = this.game.gamePlays;
-      this.totalYards = this.gamePlaysArray
+      this.gamePlaysArray.data = this.game.gamePlays;
+      this.totalYards = this.gamePlaysArray.data
         .map((p) => p.result * 1)
         .reduce((acc, value) => acc + value, 0);
-      this.avgYards = (this.totalYards / this.gamePlaysArray.length).toFixed(2);
-      this.gamePlaysArray.map((gamePlay: GamePlay) => {
+      this.avgYards = (
+        this.totalYards / this.gamePlaysArray.data.length
+      ).toFixed(2);
+      this.gamePlaysArray.data.map((gamePlay: GamePlay) => {
         if (gamePlay.play.runPass === "Run") {
           this.runPlays.push(gamePlay);
-          this.totalRushYards = this.runPlays
-            .map((p) => p.result * 1)
-            .reduce((acc, value) => acc + value, 0);
         } else {
           this.passPlays.push(gamePlay);
-          this.totalPassYards = this.runPlays
-            .map((p) => p.result * 1)
-            .reduce((acc, value) => acc + value, 0);
         }
       });
+      this.totalRushYards = this.runPlays
+        .map((p) => p.result * 1)
+        .reduce((acc, value) => acc + value, 0);
+      this.totalPassYards = this.passPlays
+        .map((p) => p.result * 1)
+        .reduce((acc, value) => acc + value, 0);
+      this.runAvg = this.totalRushYards / this.runPlays.length;
+      this.passAvg = this.totalPassYards / this.passPlays.length;
     });
     this.playsService.getPlays().subscribe((result) => {
       result.map((play) => {
@@ -78,7 +86,7 @@ export class GameComponent implements OnInit {
       this.playsArray.data.map((play) => {
         const playName = play.fullPlay;
         const gamePlays: Array<any> = [];
-        this.gamePlaysArray.map((gamePlay) => {
+        this.gamePlaysArray.data.map((gamePlay) => {
           if (gamePlay.play.fullPlay === playName) {
             gamePlays.push(gamePlay);
           }
@@ -105,12 +113,14 @@ export class GameComponent implements OnInit {
       outcome: new FormControl(null),
       gamePlays: new FormControl(),
     });
+    this.gamePlaysArray.paginator = this.tableTwoPaginator;
     this.playsArray.paginator = this.tableOnePaginator;
     this.playsArray.sort = this.tableOneSort;
+    console.log(this.playCats);
   }
 
   delete(element: any) {
-    this.gamePlaysArray = this.gamePlaysArray.filter(function (obj) {
+    this.gamePlaysArray.data = this.gamePlaysArray.data.filter(function (obj) {
       return obj.date !== element.date;
     });
     this.gameForm.patchValue({
@@ -119,26 +129,45 @@ export class GameComponent implements OnInit {
       score: this.game.score,
       location: this.game.location,
       outcome: this.game.outcome,
-      gamePlays: this.gamePlaysArray,
+      gamePlays: this.gamePlaysArray.data,
     });
     this.gamesService.updateGame(this.gameId, this.gameForm.value);
   }
 
   saveGamePlay() {
+    this.runPlays = [];
+    this.passPlays = [];
     this.gamePlayForm.patchValue({
       date: new Date(),
     });
-    this.gamePlaysArray.push(this.gamePlayForm.value);
+    this.gamePlaysArray.data.push(this.gamePlayForm.value);
     this.gameForm.patchValue({
       team: this.game.team,
       date: this.game.date,
       score: this.game.score,
       location: this.game.location,
       outcome: this.game.outcome,
-      gamePlays: this.gamePlaysArray,
+      gamePlays: this.gamePlaysArray.data,
     });
     this.gamesService.updateGame(this.gameId, this.gameForm.value);
     this.gamePlayForm.reset();
+    this.playsArray.data = this.selectPlays;
+    this.playsArray.data.map((play) => {
+      const playName = play.fullPlay;
+      const gamePlays: Array<any> = [];
+      this.gamePlaysArray.data.map((gamePlay) => {
+        if (gamePlay.play.fullPlay === playName) {
+          gamePlays.push(gamePlay);
+        }
+        if (gamePlays.length) {
+          play.gameAvg =
+            gamePlays
+              .map((p) => p.result * 1)
+              .reduce((acc, value) => acc + value, 0) / gamePlays.length;
+          play.gameCount = gamePlays.length;
+        }
+      });
+    });
   }
 
   applyFilterOne(filterValue: string) {
