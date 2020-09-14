b6a300025d76ac5a2d9081a3bcd2e7ee4f7e90a2
Mitchell Glazier <glazierm@gmail.com>
2020-09-14 14:28:17 -0600

    dashboard updates to run pass

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -33,65 +33,17 @@ export class DashboardComponent implements OnInit, OnDestroy {
   passAvg!: string;
   totalPassYards!: number;
 
-  sweepPlays: GamePlay[] = [];
-  sweepYards!: number;
-  sweepAvg!: string;
+  rightRunYards!: number;
+  rightRunAvg!: string;
 
-  trapPlays: GamePlay[] = [];
-  trapYards!: number;
-  trapAvg!: string;
+  leftRunYards!: number;
+  leftRunAvg!: string;
 
-  bootPlays: GamePlay[] = [];
-  bootYards!: number;
-  bootAvg!: string;
+  rightPassYards!: number;
+  rightPassAvg!: string;
 
-  popPassPlays: GamePlay[] = [];
-  popPassYards!: number;
-  popPassAvg!: string;
-
-  wedgePlays: GamePlay[] = [];
-  wedgeYards!: number;
-  wedgeAvg!: string;
-
-  crossBlockPlays: GamePlay[] = [];
-  crossBlockYards!: number;
-  crossBlockAvg!: string;
-
-  counterPlays: GamePlay[] = [];
-  counterYards!: number;
-  counterAvg!: string;
-
-  bellyPlays: GamePlay[] = [];
-  bellyYards!: number;
-  bellyAvg!: string;
-
-  downPlays: GamePlay[] = [];
-  downYards!: number;
-  downAvg!: string;
-
-  keepPassPlays: GamePlay[] = [];
-  keepPassYards!: number;
-  keepPassAvg!: string;
-
-  reversePlays: GamePlay[] = [];
-  reverseYards!: number;
-  reverseAvg!: string;
-
-  powerPlays: GamePlay[] = [];
-  powerYards!: number;
-  powerAvg!: string;
-
-  pass90Plays: GamePlay[] = [];
-  pass90Yards!: number;
-  pass90Avg!: string;
-
-  specialPlays: GamePlay[] = [];
-  specialYards!: number;
-  specialAvg!: string;
-
-  screenPlays: GamePlay[] = [];
-  screenYards!: number;
-  screenAvg!: string;
+  leftPassYards!: number;
+  leftPassAvg!: string;
 
   qbPlays: GamePlay[] = [];
   lhbPlays: GamePlay[] = [];
@@ -240,37 +192,6 @@ export class DashboardComponent implements OnInit, OnDestroy {
         } else {
           this.seasonPassPlays.push(gamePlay);
         }
-        // if (gamePlay.play.playCat === "Sweep") {
-        //   this.sweepPlays.push(gamePlay);
-        // } else if (gamePlay.play.playCat === "Trap") {
-        //   this.trapPlays.push(gamePlay);
-        // } else if (gamePlay.play.playCat === "Boot") {
-        //   this.bootPlays.push(gamePlay);
-        // } else if (gamePlay.play.playCat === "Pop Pass") {
-        //   this.popPassPlays.push(gamePlay);
-        // } else if (gamePlay.play.playCat === "Wedge") {
-        //   this.wedgePlays.push(gamePlay);
-        // } else if (gamePlay.play.playCat === "Cross Block") {
-        //   this.crossBlockPlays.push(gamePlay);
-        // } else if (gamePlay.play.playCat === "Counter") {
-        //   this.counterPlays.push(gamePlay);
-        // } else if (gamePlay.play.playCat === "Belly") {
-        //   this.bellyPlays.push(gamePlay);
-        // } else if (gamePlay.play.playCat === "Down") {
-        //   this.downPlays.push(gamePlay);
-        // } else if (gamePlay.play.playCat === "Keep Pass") {
-        //   this.keepPassPlays.push(gamePlay);
-        // } else if (gamePlay.play.playCat === "Reverse") {
-        //   this.reversePlays.push(gamePlay);
-        // } else if (gamePlay.play.playCat === "Power") {
-        //   this.powerPlays.push(gamePlay);
-        // } else if (gamePlay.play.playCat === "Pass") {
-        //   this.pass90Plays.push(gamePlay);
-        // } else if (gamePlay.play.playCat === "Special") {
-        //   this.specialPlays.push(gamePlay);
-        // } else if (gamePlay.play.playCat === "Screen") {
-        //   this.screenPlays.push(gamePlay);
-        // }
         if (gamePlay.play.primaryPos === "QB") {
           this.qbPlays.push(gamePlay);
         } else if (gamePlay.play.primaryPos === "Left HB") {
@@ -332,73 +253,45 @@ export class DashboardComponent implements OnInit, OnDestroy {
     this.totalPassYards = this.seasonPassPlays
       .map((p) => p.result * 1)
       .reduce((acc, value) => acc + value, 0);
-    // this.sweepYards = this.sweepPlays
-    //   .map((p) => p.result * 1)
-    //   .reduce((acc, value) => acc + value, 0);
-    // this.trapYards = this.trapPlays
-    //   .map((p) => p.result * 1)
-    //   .reduce((acc, value) => acc + value, 0);
-    // this.bootYards = this.bootPlays
-    //   .map((p) => p.result * 1)
-    //   .reduce((acc, value) => acc + value, 0);
-    // this.popPassYards = this.popPassPlays
-    //   .map((p) => p.result * 1)
-    //   .reduce((acc, value) => acc + value, 0);
-    // this.wedgeYards = this.wedgePlays
-    //   .map((p) => p.result * 1)
-    //   .reduce((acc, value) => acc + value, 0);
-    // this.crossBlockYards = this.crossBlockPlays
-    //   .map((p) => p.result * 1)
-    //   .reduce((acc, value) => acc + value, 0);
-    // this.counterYards = this.counterPlays
-    //   .map((p) => p.result * 1)
-    //   .reduce((acc, value) => acc + value, 0);
-    // this.bellyYards = this.bellyPlays
-    //   .map((p) => p.result * 1)
-    //   .reduce((acc, value) => acc + value, 0);
-    // this.downYards = this.downPlays
-    //   .map((p) => p.result * 1)
-    //   .reduce((acc, value) => acc + value, 0);
-    // this.keepPassYards = this.keepPassPlays
-    //   .map((p) => p.result * 1)
-    //   .reduce((acc, value) => acc + value, 0);
-    // this.reverseYards = this.reversePlays
-    //   .map((p) => p.result * 1)
-    //   .reduce((acc, value) => acc + value, 0);
-    // this.powerYards = this.powerPlays
-    //   .map((p) => p.result * 1)
-    //   .reduce((acc, value) => acc + value, 0);
-    // this.pass90Yards = this.pass90Plays
-    //   .map((p) => p.result * 1)
-    //   .reduce((acc, value) => acc + value, 0);
-    // this.specialYards = this.specialPlays
-    //   .map((p) => p.result * 1)
-    //   .reduce((acc, value) => acc + value, 0);
-    // this.screenYards = this.screenPlays
-    //   .map((p) => p.result * 1)
-    //   .reduce((acc, value) => acc + value, 0);
     this.runAvg = (this.totalRushYards / this.seasonRunPlays.length).toFixed(2);
     this.passAvg = (this.totalPassYards / this.seasonPassPlays.length).toFixed(
       2
     );
-    // this.sweepAvg = (this.sweepYards / this.sweepPlays.length).toFixed(2);
-    // this.trapAvg = (this.trapYards / this.trapPlays.length).toFixed(2);
-    // this.bootAvg = (this.bootYards / this.bootPlays.length).toFixed(2);
-    // this.popPassAvg = (this.popPassYards / this.popPassPlays.length).toFixed(2);
-    // this.wedgeAvg = (this.wedgeYards / this.wedgePlays.length).toFixed(2);
-    // this.crossBlockAvg = (
-    //   this.crossBlockYards / this.crossBlockPlays.length
-    // ).toFixed(2);
-    // this.counterAvg = (this.counterYards / this.counterPlays.length).toFixed(2);
-    // this.bellyAvg = (this.bellyYards / this.bellyPlays.length).toFixed(2);
-    // this.downAvg = (this.downYards / this.downPlays.length).toFixed(2);
-    // this.keepPassAvg = (this.keepPassYards / this.keepPassPlays.length).toFixed(
-    //   2
-    // );
-    // this.reverseAvg = (this.reverseYards / this.reversePlays.length).toFixed(2);
-    // this.powerAvg = (this.powerYards / this.powerPlays.length).toFixed(2);
-    // this.pass90Avg = (this.pass90Yards / this.pass90Plays.length).toFixed(2);
-    // this.specialAvg = (this.specialYards / this.specialPlays.length).toFixed(2);
-    // this.screenAvg = (this.screenYards / this.screenPlays.length).toFixed(2);
+    const rightRunPlays: Array<any> = [];
+    const leftRunPlays: Array<any> = [];
+    const rightPassPlays: Array<any> = [];
+    const leftPassPlays: Array<any> = [];
+    this.seasonRunPlays.map((rp) => {
+      if (rp.play.Direction === "Right") {
+        rightRunPlays.push(rp);
+      } else if (rp.play.Direction === "Left") {
+        leftRunPlays.push(rp);
+      }
+    });
+    this.rightRunYards = rightRunPlays
+      .map((p: any) => p.result * 1)
+      .reduce((acc: any, value: any) => acc + value, 0);
+    this.rightRunAvg = (this.rightRunYards / rightRunPlays.length).toFixed(2);
+    this.leftRunYards = leftRunPlays
+      .map((p: any) => p.result * 1)
+      .reduce((acc: any, value: any) => acc + value, 0);
+    this.leftRunAvg = (this.leftRunYards / rightRunPlays.length).toFixed(2);
+    this.seasonPassPlays.map((pp) => {
+      if (pp.play.Direction === "Right") {
+        rightPassPlays.push(pp);
+      } else if (pp.play.Direction === "Left") {
+        leftPassPlays.push(pp);
+      }
+    });
+    this.rightPassYards = rightPassPlays
+      .map((p: any) => p.result * 1)
+      .reduce((acc: any, value: any) => acc + value, 0);
+    this.rightPassAvg = (this.rightPassYards / rightPassPlays.length).toFixed(
+      2
+    );
+    this.leftPassYards = leftPassPlays
+      .map((p: any) => p.result * 1)
+      .reduce((acc: any, value: any) => acc + value, 0);
+    this.leftPassAvg = (this.leftPassYards / leftPassPlays.length).toFixed(2);
   }
 }
